99fe2eab57d20ee665a9ed5573091edf60331cfc
Chayne Thrash <chaynethrash@gmail.com>
2015-04-20 15:26:49 -0500

    Removed Comment Magic
    Comments no long decide whether out code works or not.

--- adc_impl.c
+++ adc_impl.c
@@ -1,5 +1,6 @@
 #include "adc_impl.h"
 #include "pic24_all.h"
+#include <math.h>
 #define MIC_INPUT (RA0_AN)
 #define ADC_12BIT_FLAG 1
 
@@ -9,6 +10,7 @@ void config_adc()
     configADC1_ManualCH0(MIC_INPUT, 31, ADC_12BIT_FLAG );
 }
 
-float getSample(){
+float get_sample(){
     return (float) convertADC1();
 }
+
